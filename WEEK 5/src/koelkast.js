05468aadcd3e039a99bf053e6f22740be6fca54d
s.hitipeuw <werksimon@gmail.com>
2019-01-22 15:54:24 +0100

    WEEK 5. Koelkast opdracht

---
+++ WEEK 5/src/koelkast.js
@@ -0,0 +1,46 @@
+"use strict";
+
+let producten = [
+    "sla",
+    "tomaat",
+    "brocoli",
+    "wortelen",
+    "komkommer",
+    "melk",
+    "yogurt",
+    "kwark",
+    "eieren",
+    "kaas",
+    "ham",
+    "kalkoenfilet",
+    "ontbijtspek",
+    "leverworst",
+    "maaltijdsalade",
+    "pastasalade",
+    "lasagna",
+    "pizza"
+];
+
+let groenten = [];
+let vleesbeleg = [];
+let zuivel = [];
+let diversen = [];
+
+/*
+ * Maak functie(s) die de item uit de producten array
+ * naar de andere arrays kopieert.
+ *
+ * VOORBEELD:
+ * sla naar de groenten array,
+ * ham naar de vlees array,
+ * eieren naar de zuivel array,
+ * pizza naar de diversen array
+ *
+ * Zorg dat alle items een plek in de verschillende arrays krijgen
+ */
+
+// Maak slim gebruik van array functies
+
+
+
+
