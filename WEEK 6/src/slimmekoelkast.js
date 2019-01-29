d2b5334327ffce4c4fe708dcd129af025d50aadf
s.hitipeuw <werksimon@gmail.com>
2019-01-29 20:28:05 +0100

    WEEK 6.

---
+++ WEEK 6/src/slimmekoelkast.js
@@ -0,0 +1,29 @@
+"use strict";
+
+let groenten = [];
+let vleesbeleg = [];
+let zuivel = [];
+let diversen = [];
+
+let aankopen = '{' +
+    '"koeling" : [' +
+    '{"groente": "sla"},' +
+    '{"groente": "tomaat"},' +
+    '{"groente": "komkommer"},' +
+    '{"groente": "brocoli"},' +
+    '{"vleeswaren": "ham"},' +
+    '{"vleeswaren": "salami"},' +
+    '{"vleeswaren": "leverworst"},' +
+    '{"zuivel": "eieren"},' +
+    '{"vleeswaren": "ontbijtspek"},' +
+    '{"groente": "wortelen"},' +
+    '{"groente": "andijvie"}],' +
+    '"voorraad" : [' +
+    '{"schoonmaak": "vaatwastabletten"}]' +
+    '}';
+
+
+console.log(groenten);
+console.log(vleesbeleg);
+console.log(zuivel);
+console.log(diversen);
